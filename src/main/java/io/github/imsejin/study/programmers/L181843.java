40e9327a6c3b8210b070a426c49bd0c9cf169cab
imsejin <hackeres123@gmail.com>
2023-05-09 14:57:30 +0900

    Add: algorithm

---
+++ src/main/java/io/github/imsejin/study/programmers/L181843.java
@@ -0,0 +1,51 @@
+package io.github.imsejin.study.programmers;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/181843">부분 문자열인지 확인하기</a>
+ */
+public class L181843 {
+
+    public static int solve(String myString, String target) {
+        if (target.isEmpty()) {
+            return 1;
+        }
+
+        if (myString.length() < target.length()) {
+            return 0;
+        }
+
+        Queue<Character> queue = new ArrayDeque<>();
+        for (char c : myString.toCharArray()) {
+            queue.add(c);
+        }
+
+        int i = 0;
+        int matched = 0;
+
+        while (!queue.isEmpty()) {
+            Character c = queue.poll();
+
+            if (i == target.length()) {
+                break;
+            }
+
+            char targetChar = target.charAt(i);
+            if (c == targetChar) {
+                matched++;
+                i++;
+            } else {
+                matched = 0;
+            }
+        }
+
+        if (matched == target.length()) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+}
---
+++ src/test/groovy/io/github/imsejin/study/programmers/L181843Spec.groovy
@@ -0,0 +1,22 @@
+package io.github.imsejin.study.programmers
+
+import spock.lang.Specification
+
+class L181843Spec extends Specification {
+
+    def "test"() {
+        when:
+        def actual = L181843.solve(origin, target)
+
+        then:
+        actual == expected
+
+        where:
+        origin   | target  || expected
+        "banana" | "xyz"   || 0
+        "melon"  | ""      || 1
+        "banana" | "ana"   || 1
+        "apple"  | "apple" || 1
+    }
+
+}
