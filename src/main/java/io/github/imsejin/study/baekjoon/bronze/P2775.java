dc1868304e7847554abae812ad9a799f1f2bd316
imsejin <hackeres123@gmail.com>
2023-05-23 12:56:11 +0900

    Add: algorithms of baekjoon

--- src/main/java/io/github/imsejin/study/baekjoon/bronze/P2775.java
+++ src/main/java/io/github/imsejin/study/baekjoon/bronze/P2775.java
@@ -1,8 +1,14 @@
 package io.github.imsejin.study.baekjoon.bronze;
 
+import io.github.imsejin.study.meta.Problem;
+import io.github.imsejin.study.meta.Tag;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+@Problem(tags = {
+        Tag.DYNAMIC_PROGRAMMING,
+})
 public class P2775 {
 
     private static final int[][] cache = new int[15][14];
@@ -10,48 +16,54 @@ public class P2775 {
     public static void main(String[] args) throws Exception {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             int testCaseCount = Integer.parseInt(reader.readLine());
-
-            for (int i = 0; i < cache[0].length; i++) {
-                cache[0][i] = i + 1;
-            }
+            StringBuilder sb = new StringBuilder();
 
             for (int i = 0; i < testCaseCount; i++) {
                 int k = Integer.parseInt(reader.readLine());
                 int n = Integer.parseInt(reader.readLine());
 
                 int answer = solve(k, n);
-                System.out.println(answer);
+                sb.append(answer).append('\n');
             }
+
+            System.out.println(sb);
         }
     }
 
-    static int solve(int k, int n) {
-        for (int i = 0; i < cache[0].length; i++) {
+    static int solve(int floor, int room) {
+        for (int i = 0; i < room; i++) {
             cache[0][i] = i + 1;
         }
 
-        if (n == 1) return 1;
-        if (cache[k][n - 1] > 0) return cache[k][n - 1];
-
-        for (int i = 0; i < cache[k].length; i++) {
-            int sum = 0;
-            for (int j = 0; j <= i; j++) {
-                sum += cache[i][j];
-            }
-
-            cache[k][i] = sum;
+        if (room == 1) {
+            return 1;
         }
 
-        return cache[k][n - 1];
+        if (cache[floor][room - 1] > 0) {
+            return cache[floor][room - 1];
+        }
 
-//        if (k == 1) {
-//            return getResidentsOnZeroFloor(n);
-//        }
-//        return solve(k + 1, n - 1);
-    }
+        for (int i = 1; i <= floor; i++) {
+            // 캐시된 층을 다시 계산하지 않는다.
+            if (cache[i][room - 1] > 0) continue;
 
-    private static int getResidentsOnZeroFloor(int roomCount) {
-        return roomCount * (roomCount + 1) / 2;
+            for (int j = 0; j < room; j++) {
+                // 캐시된 방을 다시 계산하지 않는다.
+                if (cache[i][j] > 0) continue;
+
+                int residentCount;
+                if (j == 0) {
+                    // 각 층의 1호는 1명만 살아야 한다.
+                    residentCount = 1;
+                } else {
+                    residentCount = cache[i][j - 1] + cache[i - 1][j];
+                }
+
+                cache[i][j] = residentCount;
+            }
+        }
+
+        return cache[floor][room - 1];
     }
 
 }
--- src/test/groovy/io/github/imsejin/study/baekjoon/bronze/P2775Spec.groovy
+++ src/test/groovy/io/github/imsejin/study/baekjoon/bronze/P2775Spec.groovy
@@ -6,15 +6,21 @@ class P2775Spec extends Specification {
 
     def "test"() {
         when:
-        def actual = P2775.solve(k, n)
+        def actual = P2775.solve(floor, room)
 
         then:
         actual == expected
 
         where:
-        k | n || expected
-//        1 | 3 || 6
-        2 | 3 || 10
+        floor | room || expected
+//        1     | 3    || 6
+//        2     | 3    || 10
+//        3     | 1    || 1
+//        3     | 2    || 5
+        3     | 3    || 15
+        3     | 4    || 35
+        4     | 3    || 21
+        5     | 4    || 84
     }
 
 }
