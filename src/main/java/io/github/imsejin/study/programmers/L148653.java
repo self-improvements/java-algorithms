10f281857828bb44395b1bf45f96c434151a0f49
ImSejin <hackeres123@gmail.com>
2023-06-03 00:00:21 +0900

    Add: algorithm of programmers

---
+++ src/main/java/io/github/imsejin/study/programmers/L148653.java
@@ -0,0 +1,63 @@
+package io.github.imsejin.study.programmers;
+
+/**
+ * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/148653">마법의 엘리베이터</a>
+ */
+public class L148653 {
+
+    static int solve(int storey) {
+        int[] digits = split(storey);
+        int magicCount = 0;
+
+        int i = digits.length - 1;
+        do {
+            int number = digits[i];
+
+            if (number == 5) {
+                if (digits[i - 1] >= 5) {
+                    digits[i - 1]++;
+                }
+
+                magicCount += 5;
+            } else if (number > 5) {
+                digits[i - 1]++;
+                magicCount += 10 - number;
+            } else {
+                magicCount += number;
+            }
+
+            digits[i] = 0;
+
+            i--;
+        } while (!isOnGround(digits));
+
+        return magicCount;
+    }
+
+    private static boolean isOnGround(int[] digits) {
+        for (int digit : digits) {
+            if (digit != 0) return false;
+        }
+
+        return true;
+    }
+
+    private static int[] split(int number) {
+        int[] digits = new int[getDigitCount(number) + 1];
+
+        int index = digits.length - 1;
+        while (number > 0) {
+            digits[index] = number % 10;
+            number /= 10;
+
+            index--;
+        }
+
+        return digits;
+    }
+
+    private static int getDigitCount(int number) {
+        return (int) Math.log10(number) + 1;
+    }
+
+}
---
+++ src/test/groovy/io/github/imsejin/study/programmers/L148653Spec.groovy
@@ -0,0 +1,34 @@
+package io.github.imsejin.study.programmers
+
+import spock.lang.Specification
+
+class L148653Spec extends Specification {
+
+    def "test"() {
+        when:
+        def actual = L148653.solve(storey)
+
+        then:
+        actual == expected
+
+        where:
+        storey    | expected
+        1         | 1
+        10        | 1
+        16        | 6
+        50        | 5
+        60        | 5
+        98        | 3
+        199       | 3
+        678       | 8
+        1010      | 2
+        2554      | 16
+        5454      | 18
+        5554      | 18
+        4554      | 18
+        9257      | 12
+        99999999  | 2
+        100000000 | 1
+    }
+
+}
