dc7b997f4048cb048063bd1b2d797f3b7f0e9b4a
imsejin <hackeres123@gmail.com>
2023-07-27 11:29:29 +0900

    Add: algorithm of programmers

---
+++ src/main/java/io/github/imsejin/study/programmers/L150370.java
@@ -0,0 +1,131 @@
+package io.github.imsejin.study.programmers;
+
+import java.time.LocalDate;
+import java.util.*;
+
+/**
+ * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/150370">개인정보 수집 유효기간</a>
+ */
+public class L150370 {
+
+    static int[] solve0(String today, String[] terms, String[] privacies) {
+        LocalDate todayDate = parseDate(today);
+
+        Map<String, Integer> termMap = new HashMap<>();
+        for (String term : terms) {
+            StringTokenizer tokenizer = new StringTokenizer(term);
+            String type = tokenizer.nextToken();
+            int month = Integer.parseInt(tokenizer.nextToken());
+
+            termMap.put(type, month);
+        }
+
+        List<Integer> privaciesToDelete = new ArrayList<>();
+        for (int i = 0; i < privacies.length; i++) {
+            String privacy = privacies[i];
+
+            StringTokenizer tokenizer = new StringTokenizer(privacy);
+            LocalDate joinDate = parseDate(tokenizer.nextToken());
+            String type = tokenizer.nextToken();
+
+            Integer month = termMap.get(type);
+            LocalDate deleteDate = joinDate.plusMonths(month);
+
+            if (deleteDate.compareTo(todayDate) <= 0) {
+                privaciesToDelete.add(i + 1);
+            }
+        }
+
+        return privaciesToDelete.stream().mapToInt(it -> it).toArray();
+    }
+
+    static int[] solve1(String today, String[] terms, String[] privacies) {
+        DateString todayString = new DateString(today);
+
+        Map<String, Integer> termMap = new HashMap<>();
+        for (String term : terms) {
+            StringTokenizer tokenizer = new StringTokenizer(term);
+            String type = tokenizer.nextToken();
+            int month = Integer.parseInt(tokenizer.nextToken());
+
+            termMap.put(type, month);
+        }
+
+        List<Integer> privaciesToDelete = new ArrayList<>();
+        for (int i = 0; i < privacies.length; i++) {
+            String privacy = privacies[i];
+
+            StringTokenizer tokenizer = new StringTokenizer(privacy);
+            DateString joinDate = new DateString(tokenizer.nextToken());
+            String type = tokenizer.nextToken();
+
+            Integer month = termMap.get(type);
+            DateString deleteDate = joinDate.plusMonth(month);
+
+            if (deleteDate.compareTo(todayString) <= 0) {
+                privaciesToDelete.add(i + 1);
+            }
+        }
+
+        return privaciesToDelete.stream().mapToInt(it -> it).toArray();
+    }
+
+    // -------------------------------------------------------------------------------------------------
+
+    private static LocalDate parseDate(String date) {
+        String[] fragments = date.split("\\.");
+        int year = Integer.parseInt(fragments[0]);
+        int month = Integer.parseInt(fragments[1]);
+        int day = Integer.parseInt(fragments[2]);
+
+        return LocalDate.of(year, month, day);
+    }
+
+    private static class DateString implements Comparable<DateString> {
+        private final int year;
+
+        private final int month;
+
+        private final int day;
+
+        public DateString(String date) {
+            String[] fragments = date.split("\\.");
+            this.year = Integer.parseInt(fragments[0]);
+            this.month = Integer.parseInt(fragments[1]);
+            this.day = Integer.parseInt(fragments[2]);
+        }
+
+        private DateString(int year, int month, int day) {
+            if (year < 0) throw new IllegalArgumentException("Invalid year: " + year);
+            if (month < 1 || month > 12) throw new IllegalArgumentException("Invalid month: " + month);
+            if (day < 1 || day > 28) throw new IllegalArgumentException("Invalid day: " + day);
+
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        }
+
+        public DateString plusMonth(int monthToAdd) {
+            int month = (this.month + monthToAdd) % 12;
+            if (month == 0) month = 12;
+
+            int year = this.year;
+            if (this.month + monthToAdd > 12) {
+                year += (this.month + monthToAdd) / 12;
+            }
+
+            return new DateString(year, month, this.day);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%04d.%02d.%02d", year, month, day);
+        }
+
+        @Override
+        public int compareTo(DateString o) {
+            return this.toString().compareTo(o.toString());
+        }
+    }
+
+}
---
+++ src/test/groovy/io/github/imsejin/study/programmers/L150370Spec.groovy
@@ -0,0 +1,22 @@
+package io.github.imsejin.study.programmers
+
+import spock.lang.Specification
+
+class L150370Spec extends Specification {
+
+    def "test"() {
+        when:
+        def actual = L150370.solve0(today, terms as String[], privacies as String[])
+
+        then:
+        actual == expected as int[]
+
+        where:
+        today        | terms                  | privacies                                                                        || expected
+        "2022.05.19" | ["A 6", "B 12", "C 3"] | ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]                 || [1, 3]
+        "2020.01.01" | ["Z 3", "D 5"]         | ["2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"] || [1, 4, 5]
+        "2020.12.28" | ["A 1"]                | ["2020.12.01 A"]                                                                 || []
+        "2020.01.05" | ["B 13", "C 30"]       | ["2018.12.06 B", "2018.12.01 B", "2017.06.05 C", "2017.07.06 C"]                 || [2, 3]
+    }
+
+}
